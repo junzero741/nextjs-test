58e0efea7e72cb41e2a98fd3eae1de633ef1f9ba
junzero741 <junzero741@gmail.com>
2022-03-16 22:51:47 +0900

    [FEAT] : add staticPath and staticProp

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,23 @@
-import Head from "next/head"
-import Link from "next/link"
-import Layout, {siteTitle} from "../components/layout"
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
+import Head from "next/head";
+import Link from "next/link";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
 const useFileSystemAsDB = true;
 const useApiAsDB = true;
 
 // Static Generation
 export async function getStaticProps() {
-
-  if(useFileSystemAsDB) {
-    const staticGenerationPostsData = getSortedPostsData()
-    return {
-      props: {
-        staticGenerationPostsData
-      }
-    }
-  }
+	if (useFileSystemAsDB) {
+		const staticGenerationPostsData = getSortedPostsData();
+		return {
+			props: {
+				staticGenerationPostsData,
+			},
+		};
+	}
 }
-
 
 // Server-Side Rendering
 // export async function getServerSideProps(context) {
@@ -34,35 +32,38 @@ export async function getStaticProps() {
 //   }
 // }
 
-
-export default function HomePage({staticGenerationPostsData, serverSideRenderingPostsData}) {
-  return (
-    <Layout home>
-    <Head>
-      <title>{siteTitle}</title>
-    </Head>
-    <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-    <div style={{border: '1px solid black'}}>Read{' '} <Link href="/posts/first-post"><a>this page!</a></Link></div>
-    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
-  )
+export default function HomePage({ staticGenerationPostsData, serverSideRenderingPostsData }) {
+	return (
+		<Layout home>
+			<Head>
+				<title>{siteTitle}</title>
+			</Head>
+			<section className={utilStyles.headingMd}>
+				<p>[Your Self Introduction]</p>
+				<p>
+					(This is a sample website - you’ll be building a site like this on <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+				</p>
+			</section>
+			<div style={{ border: "1px solid black" }}>
+				Read{" "}
+				<Link href="/posts/first-post">
+					<a>this page!</a>
+				</Link>
+			</div>
+			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+				<h2 className={utilStyles.headingLg}>Blog</h2>
+				<ul className={utilStyles.list}>
+					{staticGenerationPostsData.map(({ id, date, title }) => (
+						<li className={utilStyles.listItem} key={id}>
+							{title}
+							<br />
+							{id}
+							<br />
+							{date}
+						</li>
+					))}
+				</ul>
+			</section>
+		</Layout>
+	);
 }
