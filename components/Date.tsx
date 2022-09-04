44ad7d75fb845f4e0e271ab395967aca4672baaf
junzero741 <junzero741@gmail.com>
2022-09-04 20:57:01 +0900

    [CONFIG] : git config ignoreCase false 로 변경

---
+++ components/Date.tsx
@@ -0,0 +1,11 @@
+import { parseISO, format } from 'date-fns'
+import styled from 'styled-components'
+
+export default function Date({ dateString }: { dateString: string }) {
+  const date = parseISO(dateString)
+  return <StyledTime dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</StyledTime>
+}
+
+const StyledTime = styled.time`
+  color: ${({ theme }) => theme.color.lightGray};
+`
