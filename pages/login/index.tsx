9ca26f6ce64f1eb2fb50aea4c5cdecd32a2563b4
junzero741 <junzero741@gmail.com>
2022-09-04 21:09:09 +0900

    [CHORE] : file name casing 버그 해결 위해 파일명 임시 변경2

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,5 +1,5 @@
 import FlexContainer from 'components/FlexContainer'
-import Layout from 'components/Layout'
+import Layout from 'components/Temp'
 import { Text } from 'components/Text'
 import { Introduce } from 'pages'
 
