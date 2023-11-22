d698e5245aa31e6060dd8916003ed4c0d1b12efa
junzero741 <junzero741@gmail.com>
2023-11-23 08:57:26 +0900

    add aws bucket link, markdown to html parser

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,42 @@
+import Image from 'next/image'
 import Link from 'next/link'
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import Post from '@/components/Post';
 
-export default function Home() {
+const client = new S3Client({
+  region: 'ap-northeast-2',
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS_KEY!,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!
+  }
+});
+
+export const main = async () => {
+  const command = new GetObjectCommand({
+    Bucket: "blog.resource",
+    Key: "posts/about.md",
+  });
+
+  try {
+    const response = await client.send(command);
+    // The Body object also has 'transformToByteArray' and 'transformToWebStream' methods.
+    const str = await response.Body?.transformToString();
+    return str;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+
+
+export default async function Home() {
+  const data = await main();
+
+
+
   return (
     <div className="prose dark:prose-invert">
-      준비중
+      <Post markdownString={data} />
     </div>
   )
 }
---
+++ src/content/pages/about.md
@@ -0,0 +1,38 @@
+
+안녕하세요
+---
+
+
+* first
+* second
+
+## 두번째로 큰 텍스트
+
+1. 첫번째
+2. 두번째
+3. 세번째
+  - 세번째의 서브항목
+
+
+
+```tsx
+export default function Home() {
+  return (
+    <div className="prose dark:prose-invert">
+      준비중
+      <img className='opacity-0' src={'https://raw.githubusercontent.com/deckgo/gatsby-remark-highlight-code/master/static/screenshot-carbon.png'} alt='profile'/>
+    </div>
+  )
+}
+
+```
+
+## 아래는 인용문
+
+> 남의 말이나 글에서 직접 또는 간접으로 따온 문장.
+
+
+수평선 첨부
+
+
+---
