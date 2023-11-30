20e746e8279aad97acd4dc00694026acea40c8a3
junzero741 <junzero741@gmail.com>
2023-11-30 23:41:37 +0900

    static buckey key

--- utils/awsS3Utils/index.ts
+++ utils/awsS3Utils/index.ts
@@ -16,7 +16,8 @@ type GetResource = (path: string) => Promise<GetObjectCommandOutput | undefined>
 const getResource: GetResource = async (path) => {
   console.log('GET_RESOURCE_BUCKET:: ', process.env.S3_BUCKET);
   const command = new GetObjectCommand({
-    Bucket: process.env.S3_BUCKET!,
+    Bucket: 'blog.resource',
+    // Bucket: process.env.S3_BUCKET!,
     Key: path,
   });
 
