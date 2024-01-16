11435e75a1294491d4392b85fdc4ee4d215ed225
junzero741 <junzero741@gmail.com>
2024-01-17 00:04:29 +0900

    change main ball camera to fixed position and distance to 30

--- components/three/Balls.tsx
+++ components/three/Balls.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { CameraControls, Environment, OrbitControls, Outlines } from '@react-three/drei';
+import { Environment, Outlines } from '@react-three/drei';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import * as THREE from 'three';
 
@@ -8,11 +8,10 @@ import { Physics, useSphere } from '@react-three/cannon';
 export default function Balls({ count }: { count: number }) {
 	return (
 		<>
-			<Canvas shadows gl={{ antialias: false }} dpr={[1, 1.5]} camera={{ position: [0, 0, 20], fov: 35, near: 1, far: 40 }}>
+			<Canvas shadows gl={{ antialias: false }} dpr={[1, 1.5]} camera={{ position: [0, 0, 30], fov: 35, near: 1, far: 40 }}>
 				<ambientLight intensity={0.5} />
 				<color attach='background' args={['#dfdfdf']} />
 				<spotLight intensity={1} angle={0.2} penumbra={1} position={[30, 30, 30]} castShadow shadow-mapSize={[512, 512]} />
-				<CameraControls minDistance={5} maxDistance={30} />
 				<Physics gravity={[0, 2, 0]} iterations={10}>
 					<Pointer />
 					<Clump count={count} />
@@ -61,6 +60,6 @@ function Clump({ count }: ClumpProps) {
 
 function Pointer() {
 	const viewport = useThree((state) => state.viewport);
-	const [, api] = useSphere(() => ({ type: 'Kinematic', args: [3], position: [0, 0, 0] }));
+	const [, api] = useSphere(() => ({ type: 'Kinematic', args: [2], position: [0, 0, 0] }));
 	return useFrame((state) => api.position.set((state.pointer.x * viewport.width) / 2, (state.pointer.y * viewport.height) / 2, 0));
 }
