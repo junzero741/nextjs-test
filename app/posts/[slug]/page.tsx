7cf30f37872be8fc9ec02b5bd1c3e73b100c2832
junzero741 <junzero741@gmail.com>
2024-01-20 19:49:12 +0900

    add view count to post page

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,15 +1,18 @@
+import dynamic from 'next/dynamic';
 import PostComp from '@/components/PostComp';
 import PostRepositoryImpl from '@/repository/PostRepositoryImpl';
+const PageHitsComp = dynamic(() => import('@/components/PageHitsComp'), { ssr: false });
 import { Suspense } from 'react';
 
 export default async function PostDetail({ params }: { params: { slug: string } }) {
-  const post = await PostRepositoryImpl.getPost(decodeURIComponent(params.slug));
-  
-  return (
-    <div>
-      <Suspense fallback={<>Loading...</>}>
-        <PostComp post={post} />
-      </Suspense>
-    </div>
-  )
+	const post = await PostRepositoryImpl.getPost(decodeURIComponent(params.slug));
+
+	return (
+		<div>
+			<Suspense fallback={<>Loading...</>}>
+				<PageHitsComp />
+				<PostComp post={post} />
+			</Suspense>
+		</div>
+	);
 }
--- components/PostComp.tsx
+++ components/PostComp.tsx
@@ -1,42 +1,37 @@
-"use client";
+'use client';
 import { Marked } from 'marked';
-import {markedHighlight} from "marked-highlight";
+import { markedHighlight } from 'marked-highlight';
 import hljs from 'highlight.js';
 import '@/styles/post.scss';
 import Post from '@/domain/Post';
 
 const marked = new Marked(
-  markedHighlight({
-    langPrefix: 'hljs language-',
-    highlight(code, lang) {
-      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-      return hljs.highlight(code, { language }).value;
-    }
-  })
+	markedHighlight({
+		langPrefix: 'hljs language-',
+		highlight(code, lang) {
+			const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+			return hljs.highlight(code, { language }).value;
+		},
+	})
 );
 
+export default function PostComp({ post }: { post: Post }) {
+	const html = marked.parse(post.postContent || '');
 
-export default function PostComp({post}: {post: Post}) {
-
-  const html = marked.parse(post.postContent || '');
-
-  return (
-    <div>
-      <PostTitle>{post.postName}</PostTitle>
-      <div className='markdown-body py-4' dangerouslySetInnerHTML={{__html: html}} />
-    </div>
-  )
+	return (
+		<div>
+			<PostTitle>{post.postName}</PostTitle>
+			<div className='markdown-body py-4' dangerouslySetInnerHTML={{ __html: html }} />
+		</div>
+	);
 }
 
+type PostTitleProps = {} & JSX.IntrinsicElements['div'];
 
-type PostTitleProps = {
-
-} & JSX.IntrinsicElements['div'];
-
-function PostTitle({children}: PostTitleProps) {
-  return (
-    <div className='py-4 border-b-4 font-bold border-black'>
-      <h1 className='text-4xl'>{children}</h1>
-    </div>
-  )
+function PostTitle({ children }: PostTitleProps) {
+	return (
+		<div className='py-4 border-b-4 font-bold border-black'>
+			<h1 className='text-4xl'>{children}</h1>
+		</div>
+	);
 }
