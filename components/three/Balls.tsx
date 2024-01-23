6864af1aeee7abcbb944cae227c23aeaa66d5b54
junzero741 <junzero741@gmail.com>
2024-01-23 09:30:40 +0900

    adjust camera fov and far

--- components/three/Balls.tsx
+++ components/three/Balls.tsx
@@ -8,7 +8,7 @@ import { Physics, useSphere } from '@react-three/cannon';
 export default function Balls({ count }: { count: number }) {
 	return (
 		<>
-			<Canvas shadows gl={{ antialias: false }} dpr={[1, 1.5]} camera={{ position: [0, 0, 30], fov: 35, near: 1, far: 40 }}>
+			<Canvas shadows gl={{ antialias: false }} dpr={[1, 1.5]} camera={{ position: [0, 0, 30], fov: 45, near: 1, far: 100 }}>
 				<ambientLight intensity={0.5} />
 				<color attach='background' args={['#dfdfdf']} />
 				<spotLight intensity={1} angle={0.2} penumbra={1} position={[30, 30, 30]} castShadow shadow-mapSize={[512, 512]} />
