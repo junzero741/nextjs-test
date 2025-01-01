dbc9eeffcf9e790017f8451bd406823ed9ffb41b
junzero741 <junzero741@gmail.com>
2025-01-01 20:00:56 +0900

    delete laboratory pages

--- apps/blog/app/laboratory/posts/page.tsx
+++
@@ -1,8 +0,0 @@
-import PostMetaListCompV2 from '@/components/PostMetaListCompV2';
-import { getPosts } from '@/utils/data';
-
-export default async function Posts() {
-  const posts = await getPosts();
-
-  return <div>{posts ? <PostMetaListCompV2 posts={posts} /> : <>Loading...</>}</div>;
-}
