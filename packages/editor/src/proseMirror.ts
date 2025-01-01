6478b02a2ea943a90017dd678a715100e0bcaaba
junzero741 <junzero741@gmail.com>
2025-01-01 20:32:35 +0900

    delete editor package temporarily

--- packages/editor/src/proseMirror.ts
+++
@@ -1,9 +0,0 @@
-import {schema} from "prosemirror-schema-basic"
-import {EditorState} from "prosemirror-state"
-import {EditorView} from "prosemirror-view"
-
-export function setUpEditor () {
-  let state = EditorState.create({schema})
-  let view = new EditorView(document.querySelector<HTMLDivElement>('#app')!, {state})
-}
-
